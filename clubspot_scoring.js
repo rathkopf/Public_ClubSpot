66a2388bb28fd9c26884bf5e71128e66afa6136b
Charley Rathkopf <charley@rathkopf.org>
2024-04-08 16:38:14 -0700

    added bootstrap script.

---
+++ clubspot_scoring.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         ClubSpot Scoring Fixer
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-28
+// @description  try to take over the world!
+// @author       Charley Rathkopf
+// @match        https://theclubspot.com/dashboard/regatta/*/
+// @match        https://theclubspot.com/dashboard/regatta/*/entry-list
+// @match        https://theclubspot.com/dashboard/regatta/*/entry-list/entries
+// @match        https://theclubspot.com/scoring/*/
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @require https://raw.githubusercontent.com/rathkopf/Public_ClubSpot/main/bundle.js
+// ==/UserScript==
